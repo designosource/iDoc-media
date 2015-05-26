38202e52717afc1aa46fe40f801b0c78757bfa91
Mahammed Khan <silent785@hotmail.com>
2015-05-26 12:40:53 +0200

    removed default footer

--- sites/all/themes/idocmedia/templates/page.tpl.php
+++ sites/all/themes/idocmedia/templates/page.tpl.php
@@ -239,11 +239,6 @@
       </div>
     <?php endif; ?>
 
-    <?php if ($site_name) :?>
-      <div class="copyright large-12 columns">
-        &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
-      </div>
-    <?php endif; ?>
   </footer>
   <!--/.footer-->
 
