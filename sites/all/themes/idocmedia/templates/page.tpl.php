e31ecbe08b03bb5c17066715d305cb15a8d2f792
mahammedzkhan <mkhan@prophets.be>
2015-05-27 15:08:21 +0200

    added regions for menu

--- sites/all/themes/idocmedia/templates/page.tpl.php
+++ sites/all/themes/idocmedia/templates/page.tpl.php
@@ -90,6 +90,16 @@
   </header>
   <!--/.l-header -->
 
+  <?php if (!empty($page['whatmenu'])): ?>
+      <!--.l-header-region -->
+      <section class="l-whatmenu-region row">
+        <div class="large-12 columns">
+          <?php print render($page['whatmenu']); ?>
+        </div>
+      </section>
+      <!--/.l-header-region -->
+    <?php endif; ?>
+
  <?php if (!empty($page['what'])): ?>
       <!--.l-header-region -->
       <section class="l-what-region row">
