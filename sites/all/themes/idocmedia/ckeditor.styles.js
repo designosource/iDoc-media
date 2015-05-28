ad793a51356d285ef5d00a9f1e62a4825b2856fe
bartgoris <bartgoris@me.com>
2015-05-28 14:24:03 +0200

    Ckeditor styles toegevoegd

---
+++ sites/all/themes/idocmedia/ckeditor.styles.js
@@ -0,0 +1,10 @@
+CKEDITOR.addStylesSet( 'drupal', 
+    [ /* Block Styles */ 
+        { name : 'Heading 2' , element : 'h2' }, 
+        { name : 'Heading 3' , element : 'h3' }, 
+        { name : 'Heading 4' , element : 'h4' }, 
+        { name : 'Para', element : 'p', attributes : { 'class' : 'large-6 columns' } }, 
+        { name : 'Green Button', element : 'div', attributes : { 'class' : 'ncbc-green-button' } }, 
+        { name : 'Blue Image Button', element : 'div', attributes : { 'class' : 'ncbc-image-button' } }, 
+        { name : 'Inline Quotation' , element : 'q' },
+        { name : 'Highlighted Row', element : 'tr', attributes : { 'class' : 'ncbc-highlighted-row' } } ]);
